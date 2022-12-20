7f167fcef66cac24228ffd08ecad7123ae9d9016
Brendan Ryan <brendan.ryan@pm.me>
2022-12-20 17:41:43 -0500

    Pagination added

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -17,6 +17,7 @@ import ListingItem from '../components/ListingItem'
 const Category = () => {
     const [ listings, setListings ] = useState(null)
     const [ loading, setLoading ] = useState(true)
+    const [ lastFetchedListing, setLastFetchedListing ] = useState(null)
     const params = useParams()
     useEffect(() => {
      const fetchListings = async () => {
@@ -31,6 +32,8 @@ const Category = () => {
             )
             // Execute query and get snapshot
             const querySnap = await getDocs(q)
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+            setLastFetchedListing(lastVisible)
             const listings = []
             querySnap.forEach((doc) => {
                 return listings.push({
@@ -46,6 +49,35 @@ const Category = () => {
      }
      fetchListings()   
     }, [params.categoryName])
+    // Pagination/ Load more
+    const onFetchMoreListings = async () => {
+        try {
+            // Fetch reference
+            const listingsRef = collection(db, 'listings')
+            // Create a query
+            const q = query(listingsRef, 
+                where('type', '==', params.categoryName), 
+                orderBy('timestamp', 'desc'),
+                startAfter(lastFetchedListing), 
+                limit(10)
+            )
+            // Execute query and get snapshot
+            const querySnap = await getDocs(q)
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+            setLastFetchedListing(lastVisible)
+            const listings = []
+            querySnap.forEach((doc) => {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data(),
+                })
+            })
+            setListings((prevState) => [...prevState, ...listings])
+            setLoading(false)
+        } catch (error) {
+            toast.error('Could not fetch listings')
+        }
+     }
   return (
     <div className="category">
         <header>
@@ -62,6 +94,11 @@ const Category = () => {
                 ))}
             </ul>
         </main>
+        <br />
+        <br />
+        { lastFetchedListing && (
+            <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
+        )}
         </>
         ) : (
             <p>No Listings for { params.categoryName }</p>
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -16,6 +16,7 @@ import ListingItem from '../components/ListingItem'
 const Offers = () => {
     const [ listings, setListings ] = useState(null)
     const [ loading, setLoading ] = useState(true)
+    const [ lastFetchedListing, setLastFetchedListing ] = useState(null)
     useEffect(() => {
      const fetchListings = async () => {
         try {
@@ -29,6 +30,8 @@ const Offers = () => {
             )
             // Execute query and get snapshot
             const querySnap = await getDocs(q)
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+            setLastFetchedListing(lastVisible)
             const listings = []
             querySnap.forEach((doc) => {
                 return listings.push({
@@ -44,6 +47,34 @@ const Offers = () => {
      }
      fetchListings()   
     }, [])
+    const onFetchMoreListings = async () => {
+        try {
+            // Fetch reference
+            const listingsRef = collection(db, 'listings')
+            // Create a query
+            const q = query(listingsRef, 
+                where('offer', '==', true), 
+                orderBy('timestamp', 'desc'),
+                startAfter(lastFetchedListing), 
+                limit(10)
+            )
+            // Execute query and get snapshot
+            const querySnap = await getDocs(q)
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+            setLastFetchedListing(lastVisible)
+            const listings = []
+            querySnap.forEach((doc) => {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data(),
+                })
+            })
+            setListings((prevState) => [...prevState, ...listings])
+            setLoading(false)
+        } catch (error) {
+            toast.error('Could not fetch listings')
+        }
+     }
   return (
     <div className="category">
         <header>
@@ -60,6 +91,11 @@ const Offers = () => {
                 ))}
             </ul>
         </main>
+        <br />
+        <br />
+        { lastFetchedListing && (
+            <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
+        )}
         </>
         ) : (
             <p>There are no current offers.</p>
